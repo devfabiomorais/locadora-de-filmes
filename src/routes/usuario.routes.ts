c5c47074f45e46f9bb6e9c2ed74c6878340a57de
Fabio Santos <fabio.santos@sanasa.com.br>
2024-09-05 09:22:48 -0300

    fiz o findByID

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -4,6 +4,8 @@ import usuarioController from "../controller/usuarioController";
 const routes = Router();
 
 routes.post("/usuario", usuarioController.create);
+routes.get("/usuario/id", usuarioController.findByID);
+
 routes.get("/test", (req, res) => {
     res.send("Servidor funcionando!");
   });
