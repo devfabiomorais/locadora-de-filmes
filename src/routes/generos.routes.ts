7cb5cf8db715d13eaf1868ae72c42743730d189a
Fabio Morais <127254770+devfabiomorais@users.noreply.github.com>
2024-09-17 09:12:15 -0300

    Delete src directory

--- src/routes/generos.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import generosController from "../controller/generosController";
-
-const routes = Router();
-
-routes.post("/generos", generosController.create);
-  
-
-export default routes;
